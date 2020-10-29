644857c2daecab16e964ba932e04fe9604c7014b
katebatrakova <katerynabatrakova@gmail.com>
2020-10-29 15:32:00 -0400

    Basic todo list generator created

---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+
+function App() {
+
+
+  const [text, setText] = useState("");
+  const [listArray, setListArray] = useState([])
+
+
+
+  const handleChange = (event) => {
+    const updatedText = event.target.value;
+    setText(updatedText)
+  }
+
+
+  const addItem = () => {
+    //push the new todo item to the array
+    setListArray((prev) => {
+      return [...listArray, text]
+    })
+    //clear the input 
+    setText("")
+  }
+
+
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" value={text} onChange={handleChange} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {listArray.map((item) => {
+            return <li>{item}</li>
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
